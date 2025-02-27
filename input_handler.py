57488999990adeac2a66075dcb4025140049dc0f
Moyie Roller <moyie.roller@protonmail.com>
2025-02-27 11:31:10 +0100

    finished input handler to working condition

--- input_handler.py
+++ input_handler.py
@@ -1,24 +1,23 @@
 """Input-Handler für verschiedene Eingaben"""
 
-import midinotes
 import asyncio
 import sys
 
 valid_durations = (0.0625, 0.125, 0.25, 0.5, 1.0)
-duration_needed = False
 break_loop = False
 
 if __name__ == '__main__':
+    pitch = 0
+    duration = 0.0
     pitches = []
     durations = []
     duration_needed = True
 
-async def input_pitch(message: str) -> int:
-
+def input_pitch(message: str) -> int:
     while True:
         userInput = input(message)
 
-        if userInput.lower() == 'stop':
+        if userInput.lower() == '':
             global break_loop
             break_loop = True
             break
@@ -27,43 +26,70 @@ async def input_pitch(message: str) -> int:
             print('Exiting midi-toolbox...')
             sys.exit(0)
 
-        try:
-            userInput = int(userInput)
-            userInput in range(128) == True
-        except ValueError:
-            print('Not a valid midi note!')
+        if userInput.startswith('[') and userInput.endswith(']'):
+            chord = chord_validation(userInput)
+            if chord != None:
+                return chord
+        elif userInput.startswith('[') and not userInput.endswith(']'):
+            print('ERROR: Not a chord in the correct format [note1, note2,...]!')
+            continue
         else:
-            return userInput
+            try:
+                userInput = int(userInput)
+                if userInput not in range(0, 128):
+                    raise ValueError()
+            except ValueError:
+                print('ERROR: Not a valid midi note, only numbers between 0 and 127 accepted!')
+            else:
+                return userInput
 
-
-async def input_duration(message: str) -> float:
+def input_duration(message: str) -> float:
     while True:
         userInput = input(message)
+
+        if userInput.lower() == '':
+            print('WARNING: You need to enter a duration for the last midi note or chord!')
+            continue
 
         if userInput == 'exit':
             print('Exiting midi-toolbox...')
             sys.exit(0)
 
         try:
-            userInput = float(userInput) and userInput in valid_durations == True
-            print(userInput)
+            userInput = float(userInput)
+            if userInput not in valid_durations:
+                raise ValueError()
         except (ValueError):
-            print('Please enter a valid duration!')
+            print('ERROR: Not a valid duration, only sixteenth to whole notes accepted!')
         else:
             return userInput
 
-async def main() -> None:
-    pitch = await input_pitch('Please enter a valid midi note: ')
+def chord_validation(userInput: str) -> list:
+    chord = []
+    try:
+        stripped_input = userInput.strip('[]')
+        chord_list = stripped_input.split(',')
 
-    if not break_loop:
-        pitches.append(pitch)
-        if duration_needed:
-            duration = await input_duration('Please enter a valid duration: ')
-            durations.append(duration)
+        for single_note_string in chord_list:
+            single_note = int(single_note_string)
+            if single_note not in range(0, 128):
+                raise ValueError
+            else:
+                chord.append(single_note)
+    except ValueError:
+        print('ERROR: Not a valid midi note within the chord, only numbers between 0 and 127 accepted!')
+    else:
+        return chord
 
 if __name__ == '__main__':
     while True:
-        asyncio.run(main())
-        if break_loop == True:
+        pitch = input_pitch('Please enter a valid midi note or chord, press enter to stop the input: ')
+        if not break_loop:
+            pitches.append(pitch)
+            if duration_needed:
+                duration = input_duration('Please enter a valid duration, only sixteenth to whole notes accepted: ')
+                durations.append(duration)
+        else:
             break
+
     print(pitches, durations)
